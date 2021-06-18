e56fe4f93f4b780c2e7c37ba4e86ca244f5ea32c
Gerson Jasan Rhu Botelho <45986551+gerson9511@users.noreply.github.com>
2021-06-18 16:42:42 -0400

    Segundo avance 18-06-21
    Adquisición de datos humedad de suelo

--- Riego-IoT.ino
+++ Riego-IoT.ino
@@ -6,9 +6,16 @@ char ssid[] = "RHUTEC";                        //Nombre WiFi
 char pass[] = "Cloclo**1998";                  //Contraseña WiFi
 const char* server = "api.thingspeak.com";     //Servidor ThingSpeak
 String apiKey = "T9VMRVYDXWLXXJQV";            //Llave publica ThingSpeak
-WiFiClient client; //             
+WiFiClient client; //
+                  
+int pinValue;                   
+
+int sensorState = 0;
+int sensor=0;
 
 DHT dht(DHTPIN, DHTTYPE);
+
+const int sensor_pin = A0;
 
 void sendSensor()
 {
@@ -36,10 +43,25 @@ void setup()
      }
       Serial.println("");
       Serial.println("WiFi conectado");
+      
+  timer.setInterval(1000L, sendSensor);
   Serial.begin(115200);
 }
+
+void sendTemps()
+{
+  sensor=analogRead(A0); 
+  Serial.println(sensor);
+  delay(1000);
+}
+
 void loop()
 {
+  sendTemps();
+  Serial.println(sensorState);
+
+  delay(100);
+
   float h = dht.readHumidity();
   float t = dht.readTemperature();
   
@@ -49,6 +71,38 @@ void loop()
                              postStr +="&field1=";
                              postStr += String(h);
                              postStr +="&field2=";
+                             postStr += String(t);
+                             postStr +="&field3=";
+                             postStr += String(sensor);
+                             postStr += "\r\n\r\n";
+ 
+                             client.print("POST /update HTTP/1.1\n");
+                             client.print("Host: api.thingspeak.com\n");
+                             client.print("Connection: close\n");
+                             client.print("X-THINGSPEAKAPIKEY: "+apiKey+"\n");
+                             client.print("Content-Type: application/x-www-form-urlencoded\n");
+                             client.print("Content-Length: ");
+                             client.print(postStr.length());
+                             client.print("\n\n");
+                             client.print(postStr);
+ 
+                             Serial.print("Humedad del aire: ");
+                             Serial.print(h);
+                             Serial.print(" Temperatura aire: ");
+                             Serial.print(t);
+                             Serial.print(" Humedad del Suelo: ");
+                             Serial.print(sensor);
+                             Serial.println("%. Enviado a Thingspeak.");
+                             
+                        }
+          client.stop();
+ 
+          Serial.println("Esperando...");
+  
+  // thingspeak necesita minimo 15 seg de retraso para las actualizaciones
+  delay(1000);                        
+}
+
                              postStr += String(t);
                              postStr += "\r\n\r\n";
  
